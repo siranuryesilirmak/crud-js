04a6bd675458ef0a1bf01d213385e715d6cf4fea
şira nur yeşilırmak <siranur_03@hotmail.com>
2022-12-26 13:53:18 +0300

    Local Storage eklendi.

--- app.js
+++ app.js
@@ -4,25 +4,54 @@ const input2 = document.querySelector("#input2")
 const input3 = document.querySelector("#input3")
 const listGroup = document.querySelector("#list-group")
 
-fetch("/index.json").then(
-    Response => {
-        return Response.json()
-    }).then((responseJSON) => {
-        console.log(responseJSON)
-        list(responseJSON)
+let persons = [
+    {
+        name: "ali",
+        surname: "yılmaz",
+        age:36
+    },
+    {
+        name:"mehmet",
+        surname:"çelik",
+        age:23
+    },
+    {
+        name: "fatma",
+        surname: "kerten",
+        age:42
+    },
+    {
+        name: "melisa",
+        surname: "doğan",
+        age:18
+    }
+];
 
-    })
+
+window.addEventListener("DOMContentLoaded", () => {     
+    persons = JSON.parse(localStorage.getItem("persons")) || persons
+    persons.forEach( person =>  personList(person))
+})
 
 
-const list = (l) => {
-    l.map((index) =>{
+const personList = (person) => {
         const item = document.createElement("li")
         item.classList.add("list-group-item")
-        item.innerHTML=(`${index.name} ${index.surname} (${index.age})`)
+        item.innerHTML=(`${person.name} ${person.surname} (${person.age})`)
         listGroup.append(item)
-    })
+        localStorage.setItem("persons",JSON.stringify(persons))
 }
 
+let formValidation = () => {
+    createData()
+  };
+
+const createData = () => {
+    let item = {name: input1.value, surname: input2.value, age: input3.value}
+    personList(item)
+    persons.push(item)
+    localStorage.setItem("persons",JSON.stringify(persons))
+}
 
 
  
